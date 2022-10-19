7453c5d85fd2f9e0e80c1ce806b5dbbce682284d
zeroscc <zhucican@gmail.com>
2022-10-19 17:29:17 +0800

    应用管理后端

--- pre-system/src/main/java/com/xd/pre/modules/sys/controller/SysAppController.java
+++ pre-system/src/main/java/com/xd/pre/modules/sys/controller/SysAppController.java
@@ -1,9 +1,11 @@
 package com.xd.pre.modules.sys.controller;
 
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.xd.pre.common.utils.R;
 import com.xd.pre.log.annotation.SysOperaLog;
+import com.xd.pre.modules.sys.domain.SysApp;
 import com.xd.pre.modules.sys.dto.AppDTO;
 import com.xd.pre.modules.sys.service.ISysAppService;
 import org.apache.ibatis.annotations.Param;
@@ -37,12 +39,13 @@ public class SysAppController {
      *
      * @return
      */
+    @SysOperaLog(descrption = "获取应用列表")
     @GetMapping
-//    @PreAuthorize("hasAuthority('sys:app:view')")
-    public R getappList(Page page, @RequestParam String appName, @RequestParam String time) {
+    @PreAuthorize("hasAuthority('sys:app:view')")
+    public R getAppList(Page page, @RequestParam String appName, @RequestParam String time) {
         String startTime=null;
         String endTime=null;
-        if (!"null".equals(time) && null != time && !"".equals(time)) {
+        if (time.contains("中国")) {
             System.out.println("进入");
             String[] times = time.split(",");
             Date dateStartTime = new Date(times[0]);
@@ -51,19 +54,22 @@ public class SysAppController {
             startTime= sdf.format(dateStartTime);
             endTime= sdf.format(dateEndTime);
         }
-
-        return R.ok(appService.selectAppList(page,appName,startTime,endTime));
+        IPage<SysApp> sysAppIPage = appService.selectAppList(page, appName, startTime, endTime);
+        if (page.getCurrent()>sysAppIPage.getPages()){
+            return R.ok(appService.selectAppList(page.setCurrent(sysAppIPage.getPages()), appName, startTime, endTime));
+        }
+        return R.ok(sysAppIPage);
     }
 
     /**
-     * 保存应用以及菜单权限
+     * 保存应用
      *
      * @param appDto
      * @return
      */
     @SysOperaLog(descrption = "保存应用")
     @PostMapping
-//    @PreAuthorize("hasAuthority('sys:app:add')")
+    @PreAuthorize("hasAuthority('sys:app:add')")
     public R save(@RequestBody AppDTO appDto) {
         return appService.saveApp(appDto);
     }
@@ -75,26 +81,30 @@ public class SysAppController {
      */
     @SysOperaLog(descrption = "更新应用")
     @PutMapping
-//    @PreAuthorize("hasAuthority('sys:app:update')")
+    @PreAuthorize("hasAuthority('sys:app:update')")
     public R update(@RequestBody AppDTO appDto) {
         return R.ok(appService.updateApp(appDto));
     }
 
     /**
-     * 删除应用以及权限
+     * 删除应用
      * @param appId
      * @return
      */
     @SysOperaLog(descrption = "删除应用")
     @DeleteMapping("/{appId}")
-//    @PreAuthorize("hasAuthority('sys:app:delete')")
+    @PreAuthorize("hasAuthority('sys:app:delete')")
     public R delete(@PathVariable("appId") Integer appId) {
         return R.ok(appService.removeById(appId));
     }
-
+    /**
+     * 批量删除应用
+     * @param appIds
+     * @return
+     */
     @SysOperaLog(descrption = "批量删除应用")
     @DeleteMapping("/batch/{appIds}")
-//    @PreAuthorize("hasAuthority('sys:app:delete')")
+    @PreAuthorize("hasAuthority('sys:app:delete')")
     public R deleteBatch(@PathVariable("appIds") List<Integer> appIds) {
         return R.ok(appService.removeByIds(appIds));
     }
